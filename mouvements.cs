cc49e5826406fcdc244d8ee53bbf41f4ea45fbe6
Anat94 <anatole.babin@epitech.eu>
2022-01-22 00:46:01 +0100

    [ADD] Jump with bug (doublejump)

--- mouvements.cs
+++ mouvements.cs
@@ -3,16 +3,35 @@ using UnityEngine;
 public class mouvements : MonoBehaviour
 {
     public CharacterController playerController;
-
     Vector3 velocity;
+    bool isGrounded;
+
     public float speed = 5f;
     private float gravity = -9.81f;
 
-    void Update() {
-        Mouvement();
+    private int jumpCount = 0;
+    public int jumpMax = 1;
+    public int jumpHeight = 3;
+
+    public Transform groundCheck;
+    private float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+
+    void Start() {
+        Debug.Log(jumpCount);
+        Debug.Log(jumpMax);
     }
 
-    public void Mouvement() {
+    void Update()
+    {
+        GroundReset();
+        Mouvement();
+        Jump();
+    }
+
+    public void Mouvement()
+    {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
@@ -21,5 +40,27 @@ public class mouvements : MonoBehaviour
 
         velocity.y += gravity * Time.deltaTime;
         playerController.Move(velocity * Time.deltaTime);
+    }
+
+    public void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount != jumpMax)
+        {
+            jumpCount += 1;
+            velocity.y = Mathf.Sqrt(jumpHeight * gravity * -2f);
+            Debug.Log(jumpCount);
+        }
+    }
+
+    public void GroundReset()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0) {
+            velocity.y = -2f;
+        }
+        if (isGrounded) {
+            jumpCount = 0;
+        }
     }
 }
